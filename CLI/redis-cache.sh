4c9d4d0dc1526f04710d4855bfb8e51be66eae60
Lee Dale <lee.dale@ofqual.gov.uk>
2020-04-24 18:19:04 +0100

    Added Redis Cache

---
+++ CLI/redis-cache.sh
@@ -0,0 +1,1 @@
+az redis create --location uksouth --name LeeTestCache --resource-group ExamExampleResources --sku Basic --vm-size c0
---
+++ SDK/RedisCache/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisCache
+{
+    class Program
+    {
+        private static string ConnectionString = "LeeTestCache.redis.cache.windows.net:6380,password=QAOsdhy5TiUVF9Je2GRXPCZY0b+eHKgCAud9ocV4th8=,ssl=True,abortConnect=False";
+        private static Lazy<ConnectionMultiplexer> LazyConnection = new Lazy<ConnectionMultiplexer>(() => {
+            return ConnectionMultiplexer.Connect(ConnectionString);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get 
+            {
+                return LazyConnection.Value;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IDatabase cache = LazyConnection.Value.GetDatabase();
+            cache.StringSet("cacheItem1", "Some cached data 1");
+            cache.StringSet("cacheItem2", "Some cached data 2");
+            cache.KeyExpire("cacheItem1", DateTime.Now.AddMinutes(1));
+
+            string cachedValue = cache.StringGet("cacheItem1").ToString();
+
+            Console.WriteLine($"Retrieved cached value {cachedValue}");
+            Console.ReadLine();
+        }        
+    }
+}
